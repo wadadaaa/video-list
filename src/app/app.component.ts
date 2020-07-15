f1aadc903fcc9f8f00c99339f9c565cad99c25f7
wadadaaa <lopatinski@gmail.com>
2020-07-15 08:16:15 +0300

    route fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,6 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'video-test';
   links = [
-    { path: '/home', icon: 'home', title: 'Home' }
+    { path: '/video', icon: 'video', title: 'Video' }
   ];
 }
